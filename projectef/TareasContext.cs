56074823e6587a2be373ecf543158e86a6ccb090
Alejandro Zeballos <alejandro.zeballos@fundacion-jala.org>
2022-10-25 22:37:59 -0400

    add ignore camp

--- projectef/TareasContext.cs
+++ projectef/TareasContext.cs
@@ -36,6 +36,8 @@ public class TareasContext: DbContext
             tarea.Property(p => p.PrioridadTarea);
             
             tarea.Property(p => p.FechaCreacion);
+
+            tarea.Ignore(p => p.Resumen);
         });
     }
 }
